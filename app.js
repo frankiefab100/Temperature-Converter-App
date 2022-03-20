c38a48568eedd2f9790b1d945cf531e9c9c225ae
frankiefab100 <ohaegbulamfrank@gmail.com>
2022-03-20 13:33:22 +0100

    fix conversion value

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const fahrenheitElem = document.querySelector("select #fahrenheit");
 const kelvinElem = document.querySelector("select #kelvin");
 const degree = document.querySelector("#degree");
 const convertBtn = document.querySelector("#convert-btn");
+const tempType = document.querySelector("#type");
 
 window.addEventListener("load", () => {
   degree.value = "";
@@ -16,11 +17,11 @@ convertBtn.addEventListener("click", (e) => {
 function getCelsiusValue() {
   let inputValue = degree.value;
 
-  if (fahrenheitElem) {
-    let FahrenheitToCelsius = (inputValue - 32) * 0.55555;
-    celsiusElem.innerHTML = `${FahrenheitToCelsius} &deg;c`;
-  } else {
-    let KelvinToCelsius = inputValue - 273;
+  if (tempType.value == "fahrenheit") {
+    const FahrenheitToCelsius = (inputValue - 32) * (5 / 9);
+    celsiusElem.innerHTML = `${FahrenheitToCelsius.toFixed(4)} &deg;c`;
+  } else if (tempType.value == "kelvin") {
+    const KelvinToCelsius = inputValue - 273;
     celsiusElem.innerHTML = `${KelvinToCelsius} &deg;c`;
   }
 }
